8d016ccb8387314e3a864a52f944a13d67d72ae4
Wayne <weijing329@outlook.com>
2017-02-06 17:48:39 +0800

    理賠流程狀態變成 event log 多拋出可以辨識理賠案件的ID

--- app/contracts/insurance/ClaimRecord.sol
+++ app/contracts/insurance/ClaimRecord.sol
@@ -101,7 +101,7 @@ contract ClaimRecord is TableRowDataStorage {
   }
 
   // for other Contract
-  event e_SetStatusCode(bytes32 indexed row_CPK_hash, uint status_code);
+  event e_SetStatusCode(bytes32 indexed row_CPK_hash, string row_CPK, uint status_code);
   function Set_status_code(string row_CPK, uint status_code) {
     e_SetStatusCode(keccak256(row_CPK), status_code);
   }
